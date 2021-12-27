9988c4820006fb57b07f10a9b815664664a7f053
David Mahbubi <ulrichdavid0370@gmail.com>
2021-12-27 18:23:15 +0700

    Tambah kelas vehicle

---
+++ lib/src/mixins/SimpleDialogMixin.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// mixin SimpleDialog {
+  
+//   void showSimpleDialog(BuildContext context) {
+//     showDialog(context: context, builder: (BuildContext context) {
+//       return SimpleDialog(
+//         children: [
+//           Text('')
+//         ],
+//       );
+//     });
+//   }
+// }
---
+++ lib/src/models/vehicle.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Vehicle {
+  BuildContext context;
+
+  Vehicle(this.context);
+
+  void gantiOli() {}
+
+  void kompaBan() {}
+
+  void tambalBan() {}
+
+  void isiBahanBakar() {}
+}
