f84269c3e12bbcc848f068ab0af28ebfbe3b23db
David Mahbubi <ulrichdavid0370@gmail.com>
2022-01-22 22:48:26 +0700

    Finalizing

--- lib/src/models/services/service_mobil.dart
+++ lib/src/models/services/service_mobil.dart
@@ -1,11 +1,7 @@
 import 'package:bengkol_app/src/models/services/layanan.dart';
 import 'package:bengkol_app/src/models/services/service_motor_gigi.dart';
 
-<<<<<<< HEAD
- abstract class ServiceMobil extends Layanan {
-=======
 abstract class ServiceMobil extends Layanan {
->>>>>>> 4b1cc64010fdc46f5eee07718ca5a0f7c7eead67
   ServiceMobil(List<String> sop, String tipeLayanan, String nomorPlat)
       : super([
           'Pemeriksaan suspensi depan dan belakang',
@@ -15,11 +11,5 @@ abstract class ServiceMobil extends Layanan {
     sop += sop;
   }
 
-<<<<<<< HEAD
-    void serviceKampasKopling();
-  }
-
-=======
   void servisKampasKopling(int index);
 }
->>>>>>> 4b1cc64010fdc46f5eee07718ca5a0f7c7eead67
